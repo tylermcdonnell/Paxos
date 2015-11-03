f33742b91d3becc4fa517420eb75e95cab018afd
mikefeilbach <mikefeilbach@gmail.com>
2015-11-02 21:47:45 -0600

    Made ballot class

---
+++ PaxosHandoutLite/Ballot/BallotGenerator.java
@@ -0,0 +1,22 @@
+package Ballot;
+
+/**
+ * Generates ballots for any given leader in the Paxos algorithm.
+ * @author Mike Feilbach
+ *
+ */
+public class BallotGenerator
+{
+	private int leaderId;
+	
+	public BallotGenerator(int leaderId)
+	{
+		this.leaderId = leaderId;
+	}
+	
+	public int getNextBallot()
+	{
+		// TODO
+		return 0;
+	}
+}
