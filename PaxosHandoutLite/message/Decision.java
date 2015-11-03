11a78c09b6e4f626beb43bf558be88a19ec0ccfd
mikefeilbach <mikefeilbach@gmail.com>
2015-11-03 10:51:17 -0600

    Added and tested equals(Object o) method

--- PaxosHandoutLite/message/Decision.java
+++ PaxosHandoutLite/message/Decision.java
@@ -10,6 +10,9 @@ public class Decision extends Message
 {
 	private static final long serialVersionUID = 1L;
 	
+	// This class wraps a Proposal, but the distinct class name
+	// differentiates it from a Proposal for clarity of knowing
+	// whether a message is a Proposal or Decision.
 	private Proposal proposal;
 	
 	public Decision(Proposal p)
@@ -33,6 +36,32 @@ public class Decision extends Message
 	public int getProposalSlotNum()
 	{
 		return this.proposal.getSlotNum();
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == null)
+		{
+			return false;
+		}
+		
+		if (o instanceof Decision)
+		{
+			Decision d = (Decision) o;
+			
+			boolean slotNumEqual = this.proposal.getSlotNum() == d.proposal.getSlotNum();
+			boolean clientIdEqual = this.proposal.getCommand().getClientId() == d.proposal.getCommand().getClientId();
+			boolean cidEqual = this.proposal.getCommand().getCommandId() == d.proposal.getCommand().getCommandId();
+			boolean cmdEqual = this.proposal.getCommand().getOperation().equals(d.proposal.getCommand().getOperation());
+			
+			if (slotNumEqual && clientIdEqual && cidEqual && cmdEqual)
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 }
