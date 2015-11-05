62ead60560302782437ecbee9105d881cc8be6d6
mikefeilbach <mikefeilbach@gmail.com>
2015-11-04 19:02:27 -0600

    Scout code is done, pretty well tested

--- PaxosHandoutLite/server/Scout.java
+++ PaxosHandoutLite/server/Scout.java
@@ -2,10 +2,12 @@ package server;
 
 import java.util.ArrayList;
 
-import Ballot.Ballot;
+import ballot.Ballot;
+import message.Adopted;
 import message.Message;
 import message.P1a;
 import message.P1b;
+import message.Preempted;
 import framework.NetController;
 
 /**
@@ -58,17 +60,116 @@ public class Scout
 		}
 	}
 	
-	public void runCommander(Message m)
+	/**
+	 * This scout's run method.  When it is done completing its tasks,
+	 * the method will return false, at which point this method should
+	 * not be called again for this given Scout object.
+	 * 
+	 * @param message
+	 * 
+	 * @return false when this method should never be called again on this
+	 * Scout object.
+	 */
+	public boolean runScout(Message message)
 	{
-		if (m instanceof P1b)
+		// Scouts only listen for p1b messages.
+		if (message instanceof P1b)
 		{
-			P1b p1b = (P1b) m;
+			System.out.println("Scout got p1b");
 			
-			// TODO -- This is the only case where Scout does anything!
+			P1b p1b = (P1b) message;
+			
+			// If the ballot returned from the acceptor is the one I gave him 
+			// in p1a.
 			if (p1b.getBallot().equals(this.myBallot))
 			{
+				// If the accepted set from p1b contains any pvalues that
+				// we don't have in our pvalue set currently, add them.  In other
+				// words, take the union of the pvalue and accepted sets.
+				this.pvalues = takeUnion(this.pvalues, p1b.getAcceptedSet());
 				
+				// Testing.
+				/*System.out.print("OLD WAIT FOR LIST: ");
+				for (int i = 0; i < this.waitFor.size(); i++)
+				{
+					System.out.print(this.waitFor.get(i) + " ");
+				}
+				System.out.println();*/
+				
+				// Take this acceptor's ID off of the wait for list.
+				// Make sure we remove the Integer object, not the element
+				// at the given index (We use Integer rather than int to
+				// accomplish this.)
+				this.waitFor.remove(new Integer(p1b.getAcceptorId()));
+				
+				// Testing.
+				/*System.out.print("NEW WAIT FOR LIST: ");
+				for (int i = 0; i < this.waitFor.size(); i++)
+				{
+					System.out.print(this.waitFor.get(i) + " ");
+				}
+				System.out.println();*/
+				
+				// If |waitFor| < (|acceptors| / 2).  In other words, do we 
+				// have majority of acceptors saying they like our ballot?
+				// Make sure we cast to doubles first, we don't want truncation.
+				// (Or does this matter?  Either way, using doubles is safe.)
+				if (((double) this.waitFor.size()) < ((double) this.numServers) / 2)
+				{
+					// The majority of the acceptors like our ballot.
+					// Send to the leader that this ballot was adopted, along 
+					// with the pvalue set, so we can see if previous values 
+					// were accepted already.  We must pass a deep copy of the
+					// pvalues set and the Ballot, since we will nullify this 
+					// scout's reference, and it will be eaten by the garbage
+					// collector.
+					Adopted adopted = new Adopted(Ballot.deepCopyBallot(this.myBallot), PValue.deepCopyPValueSet(this.pvalues));
+					this.network.sendMsgToServer(this.myLeaderId, adopted);
+					
+					// This Scout is done with its tasks.
+					return false;
+				}
+			}
+			else
+			{
+				// An acceptor returned a ballot which did not match.
+				Preempted preempted = new Preempted(Ballot.deepCopyBallot(this.myBallot));
+				
+				// send to leader: <preempted, b'>
+				this.network.sendMsgToServer(this.myLeaderId, preempted);
+				
+				// This Scout is done with its tasks.
+				return false;
 			}
 		}
+		
+		// Default, this code should never be reached.
+		return true;
+	}
+	
+	
+	/**
+	 * Returns the union of the given ArrayList<PValue> sets a and b.
+	 * 
+	 * @param a, set a.
+	 * @param b, set b.
+	 * 
+	 * @return the union of the given ArrayList<PValue> sets a and b.
+	 */
+	private ArrayList<PValue> takeUnion(ArrayList<PValue> a, ArrayList<PValue> b)
+	{
+		// Traverse set b.
+		for (int i = 0; i < b.size(); i++)
+		{
+			// If set a doesn't have this item, add it.
+			PValue currVal = b.get(i);
+			
+			if (!a.contains(currVal))
+			{
+				a.add(currVal);
+			}
+		}
+		
+		return a;
 	}
 }
