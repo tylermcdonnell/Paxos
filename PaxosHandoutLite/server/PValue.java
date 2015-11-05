21adb6fbde69443c84b6cbfdd834fb81309500c7
mikefeilbach <mikefeilbach@gmail.com>
2015-11-04 19:03:35 -0600

    Added deep copy Set of PValue method

--- PaxosHandoutLite/server/PValue.java
+++ PaxosHandoutLite/server/PValue.java
@@ -1,9 +1,10 @@
 package server;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+import ballot.Ballot;
 import client.Command;
-import Ballot.Ballot;
 
 
 /**
@@ -42,6 +43,7 @@ public class PValue implements Serializable
 		return this.command;
 	}
 	
+	
 	@Override
 	public String toString()
 	{
@@ -50,5 +52,66 @@ public class PValue implements Serializable
 				+ this.slotNumber + ", " + this.command + ">";
 		return retVal;
 	}
+	
+	
+	/**
+	 * Returns a deep copy of the given ArrayList<PValue>.
+	 * 
+	 * @param ballot, the given ArrayList<PValue>.
+	 * 
+	 * @return a deep copy of the given ArrayList<PValue>.
+	 */
+	public static ArrayList<PValue> deepCopyPValueSet(ArrayList<PValue> pvalues)
+	{
+		ArrayList<PValue> newPvalues = new ArrayList<PValue>();
+		
+		for (int i = 0; i < pvalues.size(); i++)
+		{
+			// Values to deep copy:
+			Ballot oldBallot = pvalues.get(i).getBallot();
+			int oldSlotNumber = pvalues.get(i).getSlotNumber();
+			Command oldCommand = pvalues.get(i).getCommand();
+			
+			Ballot newBallot = Ballot.deepCopyBallot(oldBallot);
+			int newSlotNumber = oldSlotNumber;
+			Command newCommand = Command.deepCopyCommand(oldCommand);
+			
+			newPvalues.add(new PValue(newBallot, newSlotNumber, newCommand));
+		}
+		
+		// Testing.  Quick check.
+		//for (int i = 0; i < pvalues.size(); i++)
+		//{
+			//System.out.println("NEW PVALUE SET, element " + i + ", should be true: " + newPvalues.get(i).equals(pvalues.get(i)));
+			//System.out.println("NEW PVALUE SET, element " + i + ", should be false: " + (newPvalues.get(i) == pvalues.get(i)));
+		//}
 
+		return newPvalues;
+	}
+	
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == null)
+		{
+			return false;
+		}
+		
+		if (o instanceof PValue)
+		{
+			PValue pv = (PValue) o;
+			
+			boolean ballotEqual = this.getBallot().equals(pv.getBallot());
+			boolean slotNumEqual = this.getSlotNumber() == pv.getSlotNumber();
+			boolean cmdEqual = this.getCommand().equals(pv.getCommand());
+			
+			if (ballotEqual && slotNumEqual && cmdEqual)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
