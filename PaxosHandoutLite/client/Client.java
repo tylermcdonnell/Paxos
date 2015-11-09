f7708d9ffc826362001245c9f9f57adf55df013d
mikefeilbach <mikefeilbach@gmail.com>
2015-11-09 14:03:00 -0600

    Wait 100 seconds before re-request command

--- PaxosHandoutLite/client/Client.java
+++ PaxosHandoutLite/client/Client.java
@@ -48,7 +48,7 @@ public class Client implements Runnable {
 	
 	// If we haven't received a response for a given command within
 	// this amount of time (in milliseconds), re-send the command.
-	private static final int RESPONSE_RESEND_PERIOD = 10000;
+	private static final int RESPONSE_RESEND_PERIOD = 100000;
 	
 	/**
 	 * Constructor.
